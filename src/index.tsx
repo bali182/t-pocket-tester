22d11b29701c73cdb1823b7ad9806c87197f190a
Édes Balázs <balazs.edes@ersteinvestment.hu>
2026-06-21 20:27:59 +0200

    Added chakra

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
+import { Button, ChakraProvider, Stack, Text, defaultSystem } from "@chakra-ui/react";
 import { createRoot } from "react-dom/client";
 
 const rootElement = document.getElementById("root");
@@ -6,4 +7,13 @@ if (!rootElement) {
   throw new Error("Root element not found");
 }
 
-createRoot(rootElement).render(<div>Hello world</div>);
+createRoot(rootElement).render(
+  <ChakraProvider value={defaultSystem}>
+    <Stack align="flex-start" gap="4" p="6">
+      <Text fontSize="xl" fontWeight="semibold">
+        Hello world
+      </Text>
+      <Button colorPalette="blue">Chakra button</Button>
+    </Stack>
+  </ChakraProvider>,
+);
