2ca87ed710cbdea7c81a23e4df78b96a306a713b
Édes Balázs <balazs.edes@ersteinvestment.hu>
2026-06-27 09:50:54 +0200

    gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
+  base: command === 'build' ? (process.env.VITE_BASE_PATH ?? '/') : '/',
   plugins: [react()],
-});
+}))
