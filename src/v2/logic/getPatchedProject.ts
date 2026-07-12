a22cd3fdd9d538934f55396fcdea16ef692aed18
Édes Balázs <balazs.edes@ersteinvestment.hu>
2026-07-12 18:19:18 +0200

    patch signature

--- src/v2/logic/getPatchedProject.ts
+++ src/v2/logic/getPatchedProject.ts
@@ -1,6 +1,5 @@
 import type { ComputedProjectSchema, ProjectSchema } from '../schemas/project'
 
-export const getPatchedProject = (
-  project: ProjectSchema,
-  _computedProject: ComputedProjectSchema,
-): ProjectSchema => project
+export const getPatchedProject = (project: ProjectSchema, _computedProject: ComputedProjectSchema): ProjectSchema => {
+  return project
+}
